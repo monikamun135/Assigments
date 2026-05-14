41a0e4d0be141f17a8e4b84e103b9e7e3c7a13fe
monikamun135 <monikamun135@gmail.com>
2026-05-14 15:57:56 +0200

    Add files via upload

---
+++ Trees/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace B9
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            
+
+            List<string> animalList = new List<string>()
+            {
+                "kozica (Rupicapra rupicapra)",
+                "żubr",
+                "wilk",
+                "żbik",
+                "ryś",
+                "tchórz stepowy",
+                "norka europejska",
+                "niedźwiedź brunatny",
+                "foka szara",
+                "foka obrączkowana",
+                "foka pospolita",
+                "walenie wszystkie gatunki",
+                "podkowiec duży",
+                "podkowiec mały",
+            };
+
+           
+            ZooTree zooBinaryTree = new ZooTree();
+            for (int i = 0; i < animalList.Count; i++)
+            {
+                zooBinaryTree.Add(animalList[i]);
+                Console.WriteLine(animalList[i]);
+            }
+
+            //Wypisanie elementow drzewa
+            Console.WriteLine("InOrder Traversal:");
+            zooBinaryTree.TraverseInOrder(zooBinaryTree.Root);
+            Console.WriteLine();
+
+           
+            // #dodawanie nowego zwierzęcia do drzewa (2 pkt),
+            string animalToAdd = "";
+            Console.WriteLine("Podaj nazwę zwierzęcia które przyjechło do Zoo: ");
+            animalToAdd = Console.ReadLine();
+            if(animalToAdd != "")
+                zooBinaryTree.Add(animalToAdd);
+            // wpisanie elementow drzewa
+            Console.WriteLine("InOrder Traversal:");
+            zooBinaryTree.TraverseInOrder(zooBinaryTree.Root);
+            Console.WriteLine();
+
+          
+
+            // #wyszukiwanie, czy dane zwierzę występuje w ogrodzie (2 pkt),
+            string animalToFind1 = "";
+            string animalToFind2 = "";
+            string animalToFind3 = "";
+            animalToFind1 = "żubr";   
+            animalToFind2 = "kot";  
+            Console.WriteLine("Mogę poszukać żubra a potem kota <Enter> a jak nie chcesz to podaj swoje zwierzę:");
+            animalToFind3 = Console.ReadLine();
+
+            if (animalToFind3 == "")
+            {
+                ZooTreeNode foudedNode1 = zooBinaryTree.Find(animalToFind1);
+                if (foudedNode1 != null)
+                    Console.WriteLine("Znalazłem "+ foudedNode1.Val.ToString());
+                else
+                    Console.WriteLine("NIE znalazłem " + animalToFind1);
+
+                ZooTreeNode foudedNode2 = zooBinaryTree.Find(animalToFind2);
+                if (foudedNode2 != null)
+                    Console.WriteLine("Znalazłem " + foudedNode2.Val.ToString());
+                else
+                    Console.WriteLine("NIE znalazłem " + animalToFind2);
+            }
+            else 
+            {
+                ZooTreeNode foudedNode3 = zooBinaryTree.Find(animalToFind3);
+                if (foudedNode3 != null)
+                    Console.WriteLine("Znalazłem " + foudedNode3.Val.ToString());
+                else
+                    Console.WriteLine("NIE znalazłem " + animalToFind3);
+            }
+
+            
+            // #usuwanie istniejącego zwierzęcia z drzewa
+            string animalToFree = "";
+            Console.WriteLine("Które zwierzę chcesz wypuścić na wolność: ");
+            animalToFree = Console.ReadLine();
+            zooBinaryTree.Remove(animalToFree);
+            // Write tree elemnts
+            Console.WriteLine("PreOrder Traversal:");
+            zooBinaryTree.TraversePreOrder(zooBinaryTree.Root);
+            Console.WriteLine();
+            Console.WriteLine("PostOrder");
+            zooBinaryTree.TraversePostOrder(zooBinaryTree.Root);
+            Console.WriteLine();
+
+
+            // #zapisywanie bieżącego stanu drzewa do pliku tekstowego
+            string nazwaPikuNaZoo = "C:\\Robo\\Monika\\Inf\\B9\\ZoowPliku.txt";
+            
+            // rysuj drzewo (z internetu)
+            zooBinaryTree.Root.PrintZoo(); 
+
+            // zapisz do pliku
+            try
+            {
+                zooBinaryTree.DumpTreeToFile(zooBinaryTree.Root, nazwaPikuNaZoo);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+                Environment.Exit(0); // quit the program if the file could not be opened
+            }
+        }
+ 
+
+    }
+}
---
+++ Trees/ZooTree.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace B9
+{
+    // #Proszę stworzyć klasę lub klasy przechowujące informacje o zwierzętach w ogrodzie zoologicznym 
+    //w formie binarnego drzewa poszukiwań zawierającego obiekty typu string.
+
+    //BFS
+
+
+    public class ZooTreeNode
+    {
+        public ZooTreeNode LeftNode { get; set; }
+        public ZooTreeNode RightNode { get; set; }
+        public ZooTreeNode UpNode { get; set; }
+        public string Val { get; set; }
+    }
+
+    class ZooTree
+    {
+        public ZooTreeNode Root { get; set; }
+
+        // #dodawanie nowego zwierzęcia do drzewa
+        public bool Add(string value)
+        {
+            ZooTreeNode before = null, after = this.Root;
+            
+            while (after != null)
+            {
+                before = after;
+                if (value.CompareTo(after.Val) < 0) //Is new node in left tree?  //value < after.Val
+                    after = after.LeftNode;
+                else if (value.CompareTo(after.Val) > 0) //Is new node in right tree? value > after.Val
+                    after = after.RightNode;
+                else
+                {
+                    return false;
+                }
+            }
+
+            ZooTreeNode newNode = new ZooTreeNode();
+            newNode.Val = value;
+
+            if (this.Root == null)
+                this.Root = newNode;
+            else
+            {
+                if (value.CompareTo(before.Val) < 0)      //value < before.Val)
+                    before.LeftNode = newNode;
+                else
+                    before.RightNode = newNode;
+            }
+
+            return true;
+        }
+
+        public ZooTreeNode Find(string value)
+        {
+            return this.Find(value, this.Root);
+        }
+
+        public void Remove(string value)
+        {
+            Remove(this.Root, value);
+        }
+        // #usuwanie istniejącego zwierzęcia z drzewa (3 pkt),
+        private ZooTreeNode Remove(ZooTreeNode parent, string key)
+        {
+            if (parent == null) return parent;
+
+            if (key.CompareTo(parent.Val) < 0) parent.LeftNode = Remove(parent.LeftNode, key); //key < parent.Val
+            else if (key.CompareTo(parent.Val) > 0)  // key > parent.Val)
+                parent.RightNode = Remove(parent.RightNode, key);
+
+            // if value is same as parent's value, then this is the node to be deleted  
+            else
+            {
+                if (parent.LeftNode == null)
+                    return parent.RightNode;
+                else if (parent.RightNode == null)
+                    return parent.LeftNode;
+
+                parent.Val = MinValue(parent.RightNode);
+
+                // Delete the inorder successor  
+                parent.RightNode = Remove(parent.RightNode, parent.Val);
+            }
+
+            return parent;
+        }
+        //!!!
+        private string MinValue(ZooTreeNode node)
+        {
+            string minv = node.Val;
+
+            while (node.LeftNode != null)
+            {
+                minv = node.LeftNode.Val;
+                node = node.LeftNode;
+            }
+
+            return minv;
+        }
+
+        private ZooTreeNode Find(string value, ZooTreeNode parent)
+        {
+            if (parent != null)
+            {
+                if (value.CompareTo(parent.Val) == 0) return parent;    //value == parent.Val
+                if (value.CompareTo(parent.Val) < 0)    //value < parent.Val)
+                    return Find(value, parent.LeftNode);
+                else
+                    return Find(value, parent.RightNode);
+            }
+
+            return null;
+        }
+
+        public int GetTreeDepth()
+        {
+            return this.GetTreeDepth(this.Root);
+        }
+
+        private int GetTreeDepth(ZooTreeNode parent)
+        {
+            return parent == null ? 0 : Math.Max(GetTreeDepth(parent.LeftNode), GetTreeDepth(parent.RightNode)) + 1;
+        }
+
+        public void TraversePreOrder(ZooTreeNode parent)
+        {
+            if (parent != null)
+            {
+                Console.Write(parent.Val + ", ");
+                TraversePreOrder(parent.LeftNode);
+                TraversePreOrder(parent.RightNode);
+            }
+        }
+
+        public void TraverseInOrder(ZooTreeNode parent)
+        {
+            if (parent != null)
+            {
+                TraverseInOrder(parent.LeftNode);
+                Console.Write(parent.Val + ", ");
+                TraverseInOrder(parent.RightNode);
+            }
+        }
+
+        public void TraversePostOrder(ZooTreeNode parent)
+        {
+            if (parent != null)
+            {
+                TraversePostOrder(parent.LeftNode);
+                TraversePostOrder(parent.RightNode);
+                Console.Write(parent.Val + ", ");
+            }
+        }
+        // #zapisywanie bieżącego stanu drzewa do pliku tekstowego
+        public void DumpTreeToFile(ZooTreeNode parent, string FileNamePath)
+        {
+
+            TraversePreOrderToFile(parent, FileNamePath);
+        }
+        public void TraversePreOrderToFile(ZooTreeNode parent, string FileNamePath)
+        {
+            if (parent != null)
+            {
+                string stringToWrite = parent.Val + ", ";
+                File.AppendAllText(FileNamePath, stringToWrite);
+                TraversePreOrderToFile(parent.LeftNode, FileNamePath);
+                TraversePreOrderToFile(parent.RightNode, FileNamePath);
+            }
+        }
+    
+        public void Print()
+        {
+             Root.PrintZoo();
+        }
+    }
+}
